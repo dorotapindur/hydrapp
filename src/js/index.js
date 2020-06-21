6a5f333c425aa68272738e526c8fbf0f0bc75554
Dorota Pindur <dpindur@wp.pl>
2020-06-21 16:08:15 +0200

    mowing and disappearing water working

--- src/js/index.js
+++ src/js/index.js
@@ -14,10 +14,14 @@ const addButton = document.querySelector('.add--js');
 
 const removeButton = document.querySelector('.remove--js');
 
+
+
 let quantity = 0;
 
 const key = new Date().toISOString().slice(0, 10);//key jest stringiem
 console.log(`${key}`);
+
+
 
 //checking local storage
 let entry = localStorage.getItem(key);//entry jest stringiem
@@ -31,14 +35,26 @@ let entry = localStorage.getItem(key);//entry jest stringiem
     console.log(`ilość szklanek = ${quantity}`);
  }
 
+if (quantity > 0) {
+    let water = document.querySelector('.water--js');
+    water.style.setProperty('display', 'block');
+}
 //add button
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', function(e) {
     quantity = quantity + 1;
     counter.innerHTML = `${quantity}`;
     console.log(`Dodano 1 szklankę. Ilość szklanek = ${quantity}`);
     localStorage.setItem(key, quantity);
+    //adding animated water svg path - from https://css-tricks.com/restart-css-animation/
+    let water = document.querySelector('.water--js');
+    water.classList.remove('water-animation-remove');
+    water.classList.remove('water-animation-disappear');
+    water.classList.add('water-animation-add');
+    let newone = water.cloneNode(true);
+    water.parentNode.replaceChild(newone, water);  
 })
+
 
 removeButton.addEventListener('click', () => {
     if (quantity > 0) {
@@ -46,5 +62,18 @@ removeButton.addEventListener('click', () => {
     counter.innerHTML = `${quantity}`;
     console.log(`Odjęto 1 szklankę. Ilość szklanek = ${quantity}`);
     localStorage.setItem(key, quantity);
-    }
+        if (quantity == 0) {
+            let water = document.querySelector('.water--js');
+            water.classList.remove('water-animation-remove');
+            water.classList.remove('water-animation-add');
+            water.classList.add('water-animation-disappear');
+        }
+        else {
+            let water = document.querySelector('.water--js'); 
+            water.classList.remove('water-animation-add');
+            water.classList.add('water-animation-remove');
+            let newone = water.cloneNode(true);
+            water.parentNode.replaceChild(newone, water); 
+        }
+    }    
 })
