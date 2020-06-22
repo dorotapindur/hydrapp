9dfc27508afe508c9f4098d981d3c91c6025e037
Dorota Pindur <dpindur@wp.pl>
2020-06-22 19:15:54 +0200

    trying to make list of dates (in progress)

--- src/js/statistic.js
+++ src/js/statistic.js
@@ -8,17 +8,42 @@ registerSW();
 
 const counter = document.querySelector('.counter--js');
 const addDate = document.querySelector('.date--js');
+const ulElement = document.querySelector('.list--js');
 
 let quantity = 0;
-const key = new Date().toISOString().slice(0, 10);//key jest stringiem
-console.log(`${key}`);
+const key = new Date().toLocaleString().slice(0, 10);//key jest stringiem
 
-const storage = localStorage;
-console.log(storage);
-console.log(storage.loglevel);
-
-
-const myArray = JSON.stringify(storage);
-console.log(myArray);
-
+console.log(key);
+addDate.innerHTML = key;
+//checking local storage
+let entry = localStorage.getItem(key);//entry jest stringiem
+ if (entry) {
+    quantity = parseInt(entry);//zamienia string na number
+    //increasing counter while loading page
+    let i = 0;
+    const time = setInterval(function() {       
+        counter.innerHTML = i;
+        console.log(`licznik: ${i}`);
+        if (i >= quantity) {
+            clearInterval(time);
+        }
+        i++;
+    }, 50);
+    console.log(`ilość szklanek = ${quantity}`);
+ }
+ else {
+    counter.innerHTML = quantity;
+    console.log(`ilość szklanek = ${quantity}`);
+    localStorage.setItem(key, quantity);
+ }
+//wypisywanie elementów tablicy jako listy
+const myArray = ['21', '22', '23', '24'];
+let arrayLength = myArray.length;
+console.log(arrayLength);
+for (let a=0; a < arrayLength; a++) {
+    console.log(myArray[a]);
+    const liItem = document.createElement('li');
+    ulElement.appendChild(liItem);
+    liItem.innerHTML = myArray[a];
+}
 
