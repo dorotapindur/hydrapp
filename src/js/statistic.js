0d23d0ec2b486e8d4e689b6a1f7ba3cddbcaf207
Dorota Pindur <dpindur@wp.pl>
2020-06-22 14:05:52 +0200

    meta description content added

--- src/js/statistic.js
+++ src/js/statistic.js
@@ -10,31 +10,15 @@ const counter = document.querySelector('.counter--js');
 const addDate = document.querySelector('.date--js');
 
 let quantity = 0;
-
 const key = new Date().toISOString().slice(0, 10);//key jest stringiem
 console.log(`${key}`);
 
-addDate.innerHTML = `${key}`;
+const storage = localStorage;
+console.log(storage);
+console.log(storage.loglevel);
 
-//checking local storage
-let entry = localStorage.getItem(key);//entry jest stringiem
- if (entry) {
-    quantity = JSON.parse(entry);//zamienia string na number
-    //increasing counter while loading page
-    let i = 0;
-    const time = setInterval(function() {       
-        counter.innerHTML = `${i}`;
-        console.log(`licznik: ${i}`);
 
-        if (i >= quantity) {
-            clearInterval(time);
-        }
-        i++;
-    }, 50);
+const myArray = JSON.stringify(storage);
+console.log(myArray);
 
-    console.log(`ilość szklanek = ${quantity}`);
- }
- else {
-    counter.innerHTML = `${quantity}`;
-    console.log(`ilość szklanek = ${quantity}`);
- }
+
