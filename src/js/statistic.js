95cc61131e1952074fad0c35286c7d60c9c726a4
Dorota Pindur <dpindur@wp.pl>
2020-11-12 10:46:08 +0100

    fix: statistic - working

--- src/js/statistic.js
+++ src/js/statistic.js
@@ -8,29 +8,25 @@ registerSW();
 /* place your code below */
 
 const counterElement = document.querySelector('.counter--js');
-const ulElement = document.querySelector('.list--js');
+const dateListElement = document.querySelector('.list--js');
 
 
 //local storage content put into table
 
 const storageKeys = Object.keys(localStorage);
+const storageValues = Object.values(localStorage);
+
 
 for (let i=0; i<storageKeys.length; i++) { 
     
-    if (storageKeys[i].indexOf(2020 || 2021) != -1) {
-    const liItem = document.createElement('li');
-    ulElement.appendChild(liItem);
-    liItem.innerHTML = storageKeys[i];
-    }
-}   
+    if ((storageKeys[i].indexOf(2020) != -1) || (storageKeys[i].indexOf(2020) != -1)) {
+        const liItem = document.createElement('li');
+        dateListElement.appendChild(liItem);
+        liItem.innerHTML = storageKeys[i];
 
-const storageValues = Object.values(localStorage);
-
-for (let u=0; u<storageValues.length; u++) {
-    if (storageValues[u] != 'INFO') {
         const liItemGlass = document.createElement('li');
         counterElement.appendChild(liItemGlass);
-        let quantity = storageValues[u];
+        let quantity = storageValues[i];
         let a = 0;
         const time = setInterval(function() {
             liItemGlass.innerHTML = a;
@@ -40,4 +36,4 @@ for (let u=0; u<storageValues.length; u++) {
             a++;
         }, 50);  
     }
-}
+}   
